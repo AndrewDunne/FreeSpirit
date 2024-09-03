50adc900720a39bc5b315d28e263e941084759e8
Overcast <andrewdunne3@gmail.com>
2024-09-02 19:35:35 -0700

    Added FPS setting and fixed settings reset bugs

---
+++ HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5/Development/Core/SharedPCH.Core.Exceptions.Cpp17.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/Andrew/Documents/GitHub/FreeSpirit/HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5/Development/Core/SharedDefinitions.Core.Exceptions.Cpp17.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.41.34120
+#include "C:/Users/Andrew/Documents/GitHub/FreeSpirit/HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h"
