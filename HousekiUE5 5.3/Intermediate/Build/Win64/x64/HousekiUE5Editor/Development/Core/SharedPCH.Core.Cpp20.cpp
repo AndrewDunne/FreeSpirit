210c5d00558cc31e4d3bcdb847d4ed0ad173ca05
Overcast <andrewdunne3@gmail.com>
2024-10-25 13:28:05 -0700

    Organizing landscape and sprite foliage materials

---
+++ HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5Editor/Development/Core/SharedPCH.Core.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.41.34120
+#include "C:/Users/Andrew/Documents/GitHub/FreeSpirit/HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5Editor/Development/Core/SharedPCH.Core.Cpp20.h"
---
+++ HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5Editor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/Users/Andrew/Documents/GitHub/FreeSpirit/HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5Editor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
