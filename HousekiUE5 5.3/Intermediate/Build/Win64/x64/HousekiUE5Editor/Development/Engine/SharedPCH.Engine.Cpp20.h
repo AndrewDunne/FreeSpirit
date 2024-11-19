cd864faeb8059d9e85b11b93760968e2a5c3bd30
Overcast <andrewdunne3@gmail.com>
2024-11-18 22:47:05 -0800

    Water fast footsteps, shallow water friction, new parm school theme

---
+++ HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5Editor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Users/Andrew/Documents/GitHub/FreeSpirit/HousekiUE5 5.3/Intermediate/Build/Win64/x64/HousekiUE5Editor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
